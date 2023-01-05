19452ccdab4c2151d5facfc8185d6ea791172db9
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-05 21:52:17 +0100

    work page created

---
+++ pages/work.tsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Portfolio from "../components/Portfolio";
+
+export default function work() {
+  return (
+    <>
+      <Hero heading="My Work" message="This is som eof my recent work traveling the world." />
+      <Portfolio />
+    </>
+  );
+}
