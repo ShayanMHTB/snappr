21d9d06bf901f70fe301e0f795da1e3641b08e98
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-05 21:51:59 +0100

    contact page created

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import Contact from "../components/Contact";
+
+import Hero from "../components/Hero";
+
+export default function contact() {
+  return (
+    <div>
+      <Hero heading="Contact" massage="Submit the form below for more work and quotes." />
+      <Contact />
+    </div>
+  );
+}
