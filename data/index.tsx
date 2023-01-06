718df90d2c30b9d5e979d2b3af917c08d2d85774
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-06 22:37:09 +0100

    data moved to it's own folder

---
+++ data/index.tsx
@@ -0,0 +1,9 @@
+// Website's title and description in Head-Tag
+export const HeadData = [
+  {
+    title: "Snappr",
+  },
+  {
+    description: "Photographer' portfolio to showcase his work and contact information"
+  }
+];
---
+++ data/navbar.tsx
@@ -0,0 +1,24 @@
+// Navbar links, feel free to change them and if you've created new ones
+// don't forget to create the according pages
+export const NavbarItems = [
+  {
+    id: "home",
+    title: "Home",
+    link: "/"
+  },
+  {
+    id: "gallery",
+    title: "Gallery",
+    link: "/gallery"
+  },
+  {
+    id: "work",
+    title: "Work",
+    link: "/work"
+  },
+  {
+    id: "contact",
+    title: "Contact",
+    link: "/contact"
+  },
+];
