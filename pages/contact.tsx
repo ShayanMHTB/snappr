e9dc8aababe1e3c02e284d3b88d9647b6bd65f87
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-06 22:46:28 +0100

    contact form updated

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,12 +1,16 @@
 import Contact from "../components/Contact";
-
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function contact() {
   return (
     <div>
-      <Hero heading="Contact" massage="Submit the form below for more work and quotes." />
+      <Hero 
+        heading="Contact" 
+        message="Submit the form below for more work and quotes."
+      />
       <Contact />
+      <Footer />
     </div>
   );
 }
