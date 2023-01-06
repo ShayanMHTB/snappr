03da7f9aa318aa18e49c12aadef06bb06949a0f2
Shayan Mahtabi <shayan.mhtb@gmail.com>
2023-01-06 22:45:53 +0100

    work page fixed

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,11 +1,16 @@
 import Hero from "../components/Hero";
 import Portfolio from "../components/Portfolio";
+import Footer from "../components/Footer";
 
 export default function work() {
   return (
     <>
-      <Hero heading="My Work" message="This is som eof my recent work traveling the world." />
+      <Hero 
+        heading="My Work" 
+        message="This is some of my recent work traveling the world."
+      />
       <Portfolio />
+      <Footer />
     </>
   );
 }
